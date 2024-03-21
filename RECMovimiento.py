af6eb553551339a0421956381d181debcdf7fc14
Silver <jairomiguelrodo@gmail.com>
2024-03-20 22:16:31 -0500

    Reconocimiento y seguimiento de movimiento

---
+++ RECMovimiento.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+
+video = cv2.VideoCapture(0)
+
+while True:
+    ret,frame = video.read()
+    if ret == False: break
+    
+    cv2.imshow('Frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord ('q'):
+        break
+video.release()
+    
